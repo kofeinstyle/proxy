edd91b8477bcc591dbe5f8ba7135377c422be441
Efimov Maxim <efimovmaksim@gmail.com>
2018-06-25 17:43:48 +0300

    first

---
+++ app.js
@@ -0,0 +1,19 @@
+var proxy = require('express-http-proxy');
+var app = require('express')();
+var getRawBody = require('raw-body')
+
+var host = 'http://159.224.199.76:8000';
+
+app.use('/proxy', proxy(host, {
+    parseReqBody: true,
+
+
+    proxyReqPathResolver: function(req) {
+
+        return '/api/test/test1';
+    }
+}));
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
